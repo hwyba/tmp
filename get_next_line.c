eef593ec5fa94cc1d59fa55110d4a70b9381a531
hwyba <68051820+hwyba@users.noreply.github.com>
2022-02-02 23:45:44 +0900

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -15,21 +15,21 @@
 char	*ft_readstr(int fd, char *str)
 {
 	char	*buff;
-	int		rd_bytes;
+	int		rbytes;
 
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	rd_bytes = 1;
-	while (!ft_strchr(str, '\n') && rd_bytes != 0)
+	rbytes = 1;
+	while (!ft_strchr(str, '\n') && rbytes != 0)
 	{
-		rd_bytes = read(fd, buff, BUFFER_SIZE);
-		if (rd_bytes == -1)
+		rbytes = read(fd, buff, BUFFER_SIZE);
+		if (rbytes == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
-		buff[rd_bytes] = '\0';
+		buff[rbytes] = '\0';
 		str = ft_strjoin(str, buff);
 	}
 	free(buff);
@@ -50,33 +50,3 @@ char	*get_next_line(int fd)
 	str = ft_newstr(str);
 	return (line);
 }
-
-/*int	main(void)
-{
-	char	*line;
-	int		i;
-	int		fd1;
-	int		fd2;
-	int		fd3;
-	fd1 = open("tests/test.txt", O_RDONLY);
-	fd2 = open("tests/test2.txt", O_RDONLY);
-	fd3 = open("tests/test3.txt", O_RDONLY);
-	i = 1;
-	while (i < 7)
-	{
-		line = get_next_line(fd1);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd2);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd3);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
-	return (0);
-}*/
